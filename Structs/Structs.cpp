2b84d10135029c3ae767df6daa9b6349dbf90a58
Shiva Gupta <48547715+TeDDyDev@users.noreply.github.com>
2021-10-02 22:27:40 +0530

    Update Structs.cpp

--- Structs/Structs.cpp
+++ Structs/Structs.cpp
@@ -6,10 +6,16 @@
 using namespace std;
 
 
+struct Student{
+int age,standard;
+string first_name,last_name;    
+    
+};
 int main() {
-    int a,b,c;
-    cin>>a>>b>>c;
-    cout<<a+b+c;
-     
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
     return 0;
 }
