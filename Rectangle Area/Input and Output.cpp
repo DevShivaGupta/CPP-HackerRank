1460d91f2cb77c2cdec1df3af31a2b2f2c89afe5
Shiva Gupta <48547715+TeDDyDev@users.noreply.github.com>
2021-10-02 22:49:29 +0530

    Update Input and Output.cpp

--- Rectangle Area/Input and Output.cpp
+++ Rectangle Area/Input and Output.cpp
@@ -1,15 +1,50 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
+class Rectangle{
+public:
+int len,wid;    
+void display()
+{
+cout<<len<<" "<<wid<<endl;    
+}
+};
+class RectangleArea:public Rectangle
+{
+public:
+void read_input()
+{
+    cin>>len>>wid;
+}
+void display()
+{
+    cout<<len*wid;
+}
+
+};
 
 
-int main() {
-    int a,b,c;
-    cin>>a>>b>>c;
-    cout<<a+b+c;
-     
+int main()
+{
+    /*
+     * Declare a RectangleArea object
+     */
+    RectangleArea r_area;
+    
+    /*
+     * Read the width and height
+     */
+    r_area.read_input();
+    
+    /*
+     * Print the width and height
+     */
+    r_area.Rectangle::display();
+    
+    /*
+     * Print the area
+     */
+    r_area.display();
+    
     return 0;
 }
