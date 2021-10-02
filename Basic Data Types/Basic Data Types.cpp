dd59bdec6f89c375589f5f30cc33da9b401bee4f
Shiva Gupta <48547715+TeDDyDev@users.noreply.github.com>
2021-10-02 22:15:18 +0530

    Add files via upload

---
+++ Basic Data Types/Basic Data Types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c ;
+    float f;
+    double d;
+    cin>>a>>b>>c>>f>>d;
+    cout<<fixed<<setprecision(13)<<a<<endl<<b<<endl<<c<<endl<<f<<endl<<d<<endl;
+    return 0;
+}
